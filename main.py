17abe9d6ce2d5c39a3365431699fce1aff9a29b1
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-08-27 04:32:10 -0700

    Added guarantee opening option

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ if __name__ == "__main__":
     print("Minesweeper")
     size = input("Board size? ")
     m = int(input("Mines? "))
+    open = int(input("Guarantee opening? (0/1) "))
     w, h = size.split(",")
     move = input("Move? ")
     x, y = move.split(",")
-    b, o = board.search_start(int(x), int(y), int(w), int(h), int(m))
+    b, o = board.search_start(int(x), int(y), int(w), int(h), int(m), open)
     print(board.board_to_string(b))
-    move = input("Move? ")
+    move = input("Move? (x to quit) ")
     while move != "x":
         if move[0] == "f":
             move = move[1:]
@@ -29,4 +30,4 @@ if __name__ == "__main__":
         if board.check_win(b):
             print("Winner")
             break
-        move = input("Move? ")
+        move = input("Move? (x to quit) ")
